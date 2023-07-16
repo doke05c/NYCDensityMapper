735b0b97e82682d384a8d65b6d3589250b450a15
David Oke <David Oke>
2023-07-16 10:46:08 -0400

    added residential counter

--- processing/processor.py
+++ processing/processor.py
@@ -5,7 +5,11 @@ with open('sample_UWS.geojson') as f:
     data = json.load(f)
 
     sumarea = 0
+    sumunitsres = 0
     for i in data['features']:
         area = i['properties']['lotarea']
+        unitsres = i['properties']['unitsres']
         sumarea += area
+        sumunitsres += unitsres
     print("This block has", sumarea, "sq. feet of lot area.")
+    print("This block has", sumunitsres, "residential units.")
