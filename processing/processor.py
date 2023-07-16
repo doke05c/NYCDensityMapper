816ec24efcde5eb1923db1e1a31c9d881d1f41a4
David Oke <David Oke>
2023-07-16 10:43:39 -0400

    created a perblock area counter

--- processing/processor.py
+++ processing/processor.py
@@ -1,10 +1,11 @@
 import json
-  
-f = open('sample_UWS.geojson')
-  
-data = json.load(f)
+from pprint import pprint
 
-for i in data['features']:
-    print(i)
-  
-f.close()
+with open('sample_UWS.geojson') as f:
+    data = json.load(f)
+
+    sumarea = 0
+    for i in data['features']:
+        area = i['properties']['lotarea']
+        sumarea += area
+    print("This block has", sumarea, "sq. feet of lot area.")
