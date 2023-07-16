092cbb1d17a17995e9588a2c1d8ae251c3a7a770
David Oke <David Oke>
2023-07-16 11:16:37 -0400

    starting to work with coordinates to make block polygons

--- processing/processor.py
+++ processing/processor.py
@@ -6,11 +6,14 @@ with open('sample_UWS.geojson') as f:
 
     sum_area_sqft = 0
     sum_units_res = 0
+    coords_list = []
     for i in data['features']:
         area = i['properties']['lotarea']
         units_res = i['properties']['unitsres']
+        coord = i['geometry']['coordinates']
         sum_area_sqft += area
         sum_units_res += units_res
+        coords_list.append(coord)
     print("This block has", sum_area_sqft, "sq. feet of lot area.")
     print("This block has", sum_units_res, "residential units.")
 
@@ -18,3 +21,5 @@ with open('sample_UWS.geojson') as f:
     density_acre = sum_units_res / sum_area_acre
 
     print("This block's residential density is", round(density_acre, 2), "units per acre.")
+
+    print(coords_list)
