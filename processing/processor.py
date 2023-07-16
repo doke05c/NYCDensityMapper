f4dc953460b281cd617eb9c6f8b30e74fcf7eb49
David Oke <David Oke>
2023-07-16 10:57:10 -0400

    created density calculation

--- processing/processor.py
+++ processing/processor.py
@@ -4,12 +4,17 @@ from pprint import pprint
 with open('sample_UWS.geojson') as f:
     data = json.load(f)
 
-    sumarea = 0
-    sumunitsres = 0
+    sum_area_sqft = 0
+    sum_units_res = 0
     for i in data['features']:
         area = i['properties']['lotarea']
-        unitsres = i['properties']['unitsres']
-        sumarea += area
-        sumunitsres += unitsres
-    print("This block has", sumarea, "sq. feet of lot area.")
-    print("This block has", sumunitsres, "residential units.")
+        units_res = i['properties']['unitsres']
+        sum_area_sqft += area
+        sum_units_res += units_res
+    print("This block has", sum_area_sqft, "sq. feet of lot area.")
+    print("This block has", sum_units_res, "residential units.")
+
+    sum_area_acre = sum_area_sqft / 43560
+    density_acre = sum_units_res / sum_area_acre
+
+    print("This block's residential density is", round(density_acre, 2), "units per acre.")
