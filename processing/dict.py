0819df65d89c124a7ba5ce0462a9e118744ac8ff
David Oke <David Oke>
2023-07-19 12:45:53 -0400

    updated dict model to have density value

--- processing/dict.py
+++ processing/dict.py
@@ -2,11 +2,13 @@ totaldict = {
     1525: {
         "sum_area_sqft": 0,
         "sum_units_res": 0,
-        "coords_list": []
+        "coords_list": [],
+        "density_acre": 0
     },
     1526: {
         "sum_area_sqft": 0,
         "sum_units_res": 0,
-        "coords_list": []
+        "coords_list": [],
+        "density_acre": 0
     }
 }
