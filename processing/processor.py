4b20514e7fa2aedf2f544707faba20ad5cf485f3
David Oke <David Oke>
2023-07-19 17:03:39 -0400

    not subscriptable is interesting

--- processing/processor.py
+++ processing/processor.py
@@ -20,47 +20,6 @@ def dict_block_checker(dict, block):
         "density_acre": 0
         }
 
-with open('sample_village.geojson') as f:
-    data = json.load(f)
-
-    sum_area_sqft = 0
-    sum_units_res = 0
-    coords_list = []
-    for i in data['features']:
-        area = i['properties']['lotarea']
-        units_res = i['properties']['unitsres']
-        coord = i['geometry']['coordinates'][0][0]
-        sum_area_sqft += area
-        sum_units_res += units_res
-        coords_list.append(coord)
-
-    print("This block has", sum_area_sqft, "sq. feet of lot area.")
-    print("This block has", sum_units_res, "residential units.")
-
-    sum_area_acre = sum_area_sqft / 43560
-    density_acre = sum_units_res / sum_area_acre
-
-    print("This block's residential density is", round(density_acre, 2), "units per acre.")
-
-    print(f"There are {len(coords_list)} buildings.")
-    with open("save_coords_list.json", "w") as save_file:
-        json.dump(coords_list, save_file, indent=2)
-
-    plt.figure()
-    ax = plt.gca()
-    ax.set_aspect('equal', adjustable='box')
-    for i in coords_list:
-        xs, ys = zip(*i)
-
-        # plt.plot(xs,ys, color=(1-(density_to_color(density_acre)), density_to_color(density_acre), 0)) 
-        if sum_units_res > 0:
-            plt.fill(xs,ys, color=(1-(density_to_color(density_acre)), density_to_color(density_acre), 0))
-    
-    if sum_units_res == 0:
-        print("Since this block has no residential units, it will be left out of the map.")
-    
-    plt.show()
-
 with open('1-data.json') as f:
     data = json.load(f)
 
@@ -80,7 +39,7 @@ with open('1-data.json') as f:
 
     #in a loop, calculate density by block in the super-dict
     for i in super_dict:
-        i[density_acre] = i[sum_units_res] / (i[sum_area_sqft] / 43560)
+        i["density_acre"] = i["sum_units_res"] / (i["sum_area_sqft"] / 43560)
 
     # with open("save_coords_list.json", "w") as save_file:
     #     json.dump(coords_list, save_file, indent=2)
@@ -91,9 +50,9 @@ with open('1-data.json') as f:
 
     #in a loop, render each block
     for i in super_dict:    
-        for j in i[coords_list]:
+        for j in i["coords_list"]:
             xs, ys = zip(*j)
-            if i[sum_units_res] > 0:
-                plt.fill(xs,ys, color=(1-(density_to_color(i[density_acre])), density_to_color(i[density_acre]), 0))
+            if i["sum_units_res"] > 0:
+                plt.fill(xs,ys, color=(1-(density_to_color(i["density_acre"])), density_to_color(i["density_acre"]), 0))
         
     plt.show()
