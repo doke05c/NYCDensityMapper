3b9818bd2ceb6f2565151e7d53b40da50e88790f
David Oke <David Oke>
2023-07-19 13:28:30 -0400

    fleshing out process to update mega-dictionary

--- processing/dict.py
+++ processing/dict.py
@@ -12,3 +12,17 @@ totaldict = {
         "density_acre": 0
     }
 }
+
+def dict_block_checker(dict, block):
+    if block not in dict:
+        dict[block] = {
+        "sum_area_sqft": 0,
+        "sum_units_res": 0,
+        "coords_list": [],
+        "density_acre": 0
+        }
+
+dict_block_checker(totaldict, 1527)
+totaldict[1527]["sum_area_sqft"] = 3
+dict_block_checker(totaldict, 1527)
+print(totaldict)
