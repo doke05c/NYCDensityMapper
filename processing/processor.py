bf595c284234db274d05abf1193a2c268773bddd
David Oke <David Oke>
2023-07-19 17:50:25 -0400

    seemed to have moved on from the subscriptability issue, now it is rgb that is not working

--- processing/processor.py
+++ processing/processor.py
@@ -6,7 +6,10 @@ import math
 #the more dense the housing, the greener the block is. 
 #this scales logarithmically up to 1000 units/acre
 def density_to_color(density):
-    return math.log(density,10)/3
+    if density < 1000:
+        return math.log(density,10)/3
+    else:
+        return 0.9999999999
 
 super_dict = {}#super-dictionary
 
@@ -29,16 +32,16 @@ with open('1-data.json') as f:
         dict_block_checker(super_dict, i['properties']['block'])
 
         #add to block area
-        super_dict[i['properties']['block']["sum_area_sqft"]] += i['properties']['lotarea']
+        super_dict[i['properties']['block']]["sum_area_sqft"] += i['properties']['lotarea']
         
         #add to block res unit count
-        super_dict[i['properties']['block']["sum_units_res"]] += i['properties']['unitsres'] 
+        super_dict[i['properties']['block']]["sum_units_res"] += i['properties']['unitsres'] 
 
         #add to coordinate list
-        super_dict[i['properties']['block']["coords_list"]].append(i['geometry']['coordinates'][0][0])
+        super_dict[i['properties']['block']]["coords_list"].append(i['geometry']['coordinates'][0][0])
 
     #in a loop, calculate density by block in the super-dict
-    for i in super_dict:
+    for i in super_dict.values():
         i["density_acre"] = i["sum_units_res"] / (i["sum_area_sqft"] / 43560)
 
     # with open("save_coords_list.json", "w") as save_file:
@@ -49,7 +52,7 @@ with open('1-data.json') as f:
     ax.set_aspect('equal', adjustable='box')
 
     #in a loop, render each block
-    for i in super_dict:    
+    for i in super_dict.values():    
         for j in i["coords_list"]:
             xs, ys = zip(*j)
             if i["sum_units_res"] > 0:
